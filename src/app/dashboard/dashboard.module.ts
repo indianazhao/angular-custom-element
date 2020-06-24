1cd3a594102771400041f6efe6af1eff71c5c60f
indianazhao <indiana.zhao@gmail.com>
2020-06-24 13:11:49 +0800

    npm i @angular/elements and create module 'dashboard' with 'dashboard-tie' component.

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardTileComponent } from './dashboard-tile/dashboard-tile.component';
+
+@NgModule({
+  declarations: [DashboardTileComponent],
+  imports: [
+    CommonModule
+  ],
+})
+export class DashboardModule { }
